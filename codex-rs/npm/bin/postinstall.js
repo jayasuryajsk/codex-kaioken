806458219f3fdb956b34fe6510879463c27b02cd
jayasuryajsk <kjayasurya97@gmail.com>
2025-12-05 16:37:20 +1100

    release: v0.1.7 - fix npm package bundling
    - Bump version to 0.1.7
    - Add .npmignore to exclude local binaries
    - Fix postinstall.js version
    - Update README example

--- codex-rs/npm/bin/postinstall.js
+++ codex-rs/npm/bin/postinstall.js
@@ -8,7 +8,7 @@ import fetch from 'node-fetch';
 import tar from 'tar';
 import unzipper from 'unzipper';
 
-const version = process.env.CODEX_KAIOKEN_VERSION || '0.1.6';
+const version = process.env.CODEX_KAIOKEN_VERSION || '0.1.7';
 const platform = process.platform;
 const arch = process.arch;
 
